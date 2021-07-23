f74853aee6da9e125285cdaec7a5723fc787fcca
MariaP78 <maria_minodora07@yahoo.com>
2021-07-23 13:17:41 +0300

    Working Schelet App

---
+++ src/main/java/msg/practica/ro/service/CustomUserDetailsService.java
@@ -0,0 +1,24 @@
+package msg.practica.ro.service;
+
+import msg.practica.ro.model.User;
+import msg.practica.ro.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userRepo.findByEmail(email);
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found");
+		}
+		return new CustomUserDetails(user);
+	}
+
+}
