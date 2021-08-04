28d47a241ec6899849da3f7d41be1189d54f838b
DESKTOP-2UUOSES\Alex <alex_pd133@yahoo.ro>
2021-08-04 12:22:47 +0300

    Added comment in WebConfig

--- src/main/java/msg/practica/ro/configuration/WebSecurityConfig.java
+++ src/main/java/msg/practica/ro/configuration/WebSecurityConfig.java
@@ -86,18 +86,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable()
+        //sa securizam rest end point-urile
+        http.csrf().disable();
                 // dont authenticate this particular request
-                .authorizeRequests().antMatchers("/login").permitAll().
-                // all other requests need to be authenticated
-                        anyRequest().authenticated().and().
-                // make sure we use stateless session; session won't be used to
-                // store user's state.
-                        exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+//                authorizeRequests().antMatchers("/login").permitAll().
+//                // all other requests need to be authenticated
+//                        anyRequest().authenticated().and().
+//                // make sure we use stateless session; session won't be used to
+//                // store user's state.
+//                        exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and().sessionManagement()
+//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 //
 //        // Add a filter to validate the tokens with every request
-        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+//        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
 
 
 //        http.authorizeRequests()
